ac0edbca45c05a7138250e658c0e3174c6e6f305
方勇 <silly@digitalgd.com.cn>
2025-02-25 14:59:50 +0800

    feat: 批量播放

---
+++ src/pages/words/wordBatchPlay.vue
@@ -0,0 +1,22 @@
+<script setup>
+const route = useRoute()
+const { cid } = route.query
+const result = getStore({ name: 'result' })
+const checked = getStore({ name: 'checked' })
+const words = ref([])
+result.forEach((item) => {
+  const filters = item.wordList.filter(el => checked.includes(el.word))
+  words.value = [...words.value, ...filters]
+})
+const index = ref([0])
+</script>
+
+<template>
+  <div>
+    <WordPlay :word="words[0].word" :pronunciation="words[0].pronunciation" :meaning="words[0].meaning" :cid="cid" @play-end="index++" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
